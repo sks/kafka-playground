8fec77c2bf44cf89d4c16b4f94ee74a6adaae193
Sabith Karippullil Soopy <sabithksme@gmail.com>
2023-06-05 20:51:45 -0700

    Added file appCD/modules/rds/variables.tf

---
+++ appCD/modules/rds/variables.tf
@@ -0,0 +1,27 @@
+variable "project" {
+
+}
+
+variable "env" {
+
+}
+
+variable "vpc_id" {
+
+}
+
+variable "private_subnets" {
+
+}
+
+variable "cidr_blocks" {
+
+}
+
+variable "db_username" {
+
+}
+
+variable "db_password" {
+
+}
