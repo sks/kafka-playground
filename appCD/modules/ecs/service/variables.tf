de685fc7a2d12eaf711c429c9298b30e7dc0516c
Sabith Karippullil Soopy <sabithksme@gmail.com>
2023-06-05 20:45:42 -0700

    Added file appCD/modules/ecs/service/variables.tf

---
+++ appCD/modules/ecs/service/variables.tf
@@ -0,0 +1,47 @@
+variable "name" {
+
+}
+
+variable "cluster" {
+
+}
+
+variable "task_definition" {
+
+}
+
+variable "desired_count" {
+
+}
+
+variable "launch_type" {
+
+}
+
+variable "lb_target_group" {
+    default = ""
+}
+
+variable "container_name" {
+
+}
+
+variable "container_port" {
+
+}
+
+variable "network_config" {
+  
+}
+
+variable "http_listener" {
+    default = ""
+}
+
+variable "namespace" {
+  
+}
+
+variable "dns_name" {
+  
+}
