9568e67a723fe7b822c65f89ee6c21ce298acb82
Sabith Karippullil Soopy <sabithksme@gmail.com>
2023-06-05 20:45:56 -0700

    Added file appCD/modules/ecs/service-without-lb/variables.tf

---
+++ appCD/modules/ecs/service-without-lb/variables.tf
@@ -0,0 +1,39 @@
+variable "name" {
+
+}
+
+variable "cluster" {
+
+}
+
+variable "task_definition" {
+
+}
+
+variable "desired_count" {
+
+}
+
+variable "launch_type" {
+
+}
+
+variable "container_name" {
+
+}
+
+variable "container_port" {
+
+}
+
+variable "network_config" {
+  
+}
+
+variable "namespace" {
+  
+}
+
+variable "dns_name" {
+  
+}
